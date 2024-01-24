dc0ea59a4f91c5f28594ba3dd9d60a49331a322d
YAN Jiawei <55325650+quark-strange@users.noreply.github.com>
2024-01-24 18:54:03 +0800

    Update workflow.py

--- RoCell/workflow.py
+++ RoCell/workflow.py
@@ -1,125 +1,127 @@
-import os
+import os, scipy
 import pandas as pd
+import numpy as np
 import scanpy as sc
 from itertools import combinations
 from multiprocessing import Pool
+from tqdm.auto import tqdm
+from scipy.stats import kstest, nbinom
+from sklearn.metrics.pairwise import cosine_similarity
 
 from anndata import read_h5ad
 from RoCell import utils
 
-def load_preprocessing(file_name: str, 
-                       min_genes: int, 
-                       min_counts: int, 
-                       min_cells: int):
-    
-    if file_name.endswith('h5ad') == False:
-        raise TypeError('RoCell only supports h5ad file')
-    
-    adata_raw = read_h5ad(file_name)
-    mito_genes = adata_raw.var['gene_ids'].str.startswith('MT-')
-    adata_raw.obs['n_genes'] = sum(adata_raw.X > 0, axis=1)
-    adata_raw.var['n_cells'] = sum(adata_raw.X > 0, axis=0).T
-    if type(adata_raw.X) == np.ndarray:
-        adata_raw.obs['percent_mito'] = np.sum(
-            adata_raw[:, mito_genes].X, axis=1) / np.sum(adata_raw.X, axis=1)
-        adata_raw.obs['n_counts'] = adata_raw.X.sum(axis=1)
-    else:
-        adata_raw.obs['percent_mito'] = np.sum(adata_raw[:, mito_genes].X, axis=1).A1 / np.sum(adata_raw.X, axis=1).A1
-        adata_raw.obs['n_counts'] = adata_raw.X.sum(axis=1).A1
-
-    adata = utils.qc(adata_raw, min_genes, min_counts, min_cells)
-
-    return adata
-
-
 class Workflow:
-    def __init__(self, 
-                 file_name: str,
-                 min_genes: int = 200, 
-                 min_counts: int = 1_000, 
-                 min_cells: int = 2):
-        self.adata = load_preprocessing(file_name, min_genes, min_counts, min_cells)
+    def __init__(self, file_name: str):
+        if file_name.endswith('h5ad') == False:
+            raise TypeError('RoCell only supports h5ad file')
+        else:
+            self.adata_raw = read_h5ad(file_name)
+        
+    def preprocessing(self, gene_id_col_label: str = 'gene_ids', **qc_kwargs):
+        self.gene_id_col_label = gene_id_col_label
+        
+        mito_genes = self.adata_raw.var[gene_id_col_label].str.startswith('MT-')
+        self.adata_raw.obs['n_genes'] = (self.adata_raw.X > 0).sum(axis=1)
+        self.adata_raw.var['n_cells'] = (self.adata_raw.X > 0).sum(axis=0).T
+        if type(self.adata_raw.X) == np.ndarray:
+            self.adata_raw.obs['percent_mito'] = np.sum(
+                self.adata_raw[:, mito_genes].X, axis=1) / np.sum(self.adata_raw.X, axis=1)
+            self.adata_raw.obs['n_counts'] = self.adata_raw.X.sum(axis=1)
+        else:
+            self.adata_raw.obs['percent_mito'] = np.sum(self.adata_raw[:, mito_genes].X, axis=1).A1 / np.sum(self.adata_raw.X, axis=1).A1
+            self.adata_raw.obs['n_counts'] = self.adata_raw.X.sum(axis=1).A1
+
+        self.adata = utils.qc(adata = self.adata_raw, **qc_kwargs)
         
     def compute_Pearson_residual(self, min_var_pr: float = 1.3, min_mean_exp: float = 0.0008):
         self.adata.var['qc_gene_idx'] = list(range(len(self.adata.var)))
         z = utils.analytic_pearson_residuals(scipy.sparse.csr_matrix(self.adata.X), 100)
         self.adata.var['pr_var'] = np.squeeze(np.asarray(np.var(z, axis = 0))) 
-        means_exp = np.mean(adata.X, axis = 0)
-        self.adata.var['mean_expression'] = np.squeeze(np.asarray(means_exp))
+        self.adata.var['mean_expression'] = np.squeeze(np.asarray(np.mean(self.adata.X, axis = 0)))
         self.adata_apr = self.adata.copy()
-        self.adata.X = z
+        self.adata_apr.X = np.asarray(z)
         self.aprg_mask = (self.adata.var.pr_var > min_var_pr) & (self.adata.var.mean_expression > min_mean_exp)
-        
     
-    def _get_batch_expression_descriptors(self, batch):
-            adata_X = self.adata.X
-            batch_id_a = batch
-            if 'qc_cell_idx' not in self.adata.obs.columns:
-                print('cell index is not define!')
-            else:
-                df_a = utils.get_nb_parameter_df(adata_X[self.adata.obs.loc[adata.obs[batch_label] == batch_id_a].qc_cell_idx,:], self.aprg_mask)
-                df_a.to_excel(os.path.join(path,'{}.xlsx'.format(batch_id_a)))
-                print('excel file of batch {} has saved!'.format(batch_id_a))
+    def _get_batch_feature_vectors(self, batch_id):
+        cell_filter = self.adata.obs[self.adata.obs[self.batch_label] == batch_id].qc_cell_idx
+        batch_x = self.adata.X[cell_filter][:,self.aprg_mask].toarray()
+        
+        var_h = utils.nb_fit(batch_x.mean(0), batch_x.var(0))
+        r, p = utils.nb_p(batch_x.mean(0), var_h)
+
+        feature_vectors= pd.DataFrame([kstest(batch_x[:,i], nbinom.cdf, args=(r[i],p[i]), method='exact').statistic for i in range(len(r))],
+                                        columns=['loss'])
+        feature_vectors['skewness'] = scipy.stats.nbinom.stats(r, p, moments='s')
+        feature_vectors['theta'] = r
+            
+        feature_vectors.to_excel(self.path_batch_effect_feature.format(batch_id), index=False)
+        
+        return True
                 
-    def _def get_batch_cos_sim(pair):
+    def _get_batch_cosine_similarity(self, pair):
         sample_id_a, sample_id_b = pair
-        df_a = pd.read_excel(os.path.join(path, sample_id_a + '.xlsx'))
-        df_b = pd.read_excel(os.path.join(path, sample_id_b + '.xlsx'))
-        df_ab = utils.gene_batch_effect_profiling(df_a, df_b, sample_id_a, sample_id_b)
-        df_ab.to_excel(os.path.join(path_,sample_id_a+'_vs_'+ sample_id_b +'.xlsx'))
-        print('calculation of cos between {} and {} is done and saved!'.format(sample_id_a, sample_id_b))
-
-
+        sample_id_a, sample_id_b = sample_id_a.replace('/',':'), sample_id_b.replace('/',':')
+        
+        file_name = os.path.join(self.path_cos_similarity.format(sample_id_a, sample_id_b))
+        if os.path.isfile(file_name) == False:
+            df_a = pd.read_excel(self.path_batch_effect_feature.format(sample_id_a))
+            df_b = pd.read_excel(self.path_batch_effect_feature.format(sample_id_b))
+            
+            cs = pd.DataFrame(cosine_similarity(df_a, df_b).diagonal(),
+                              columns=['{}_{}_cos_similarity'.format(sample_id_a, sample_id_b)])
+            cs.to_excel(file_name, index=False)
+        return True
                 
-    def compute_features(self, batch_label: str = 'Batch', num_cores: int = 36):
+    def compute_biological_heterogeneity(self, batch_label: str = 'Batch', num_cores: int = 36):
+        self.batch_label = batch_label
+        self.path_batch_effect_feature = './{}/batch_effect_feature'.format(batch_label)
+        if not os.path.exists(self.path_batch_effect_feature):
+            os.makedirs(self.path_batch_effect_feature)
+        self.path_batch_effect_feature = self.path_batch_effect_feature + '/{}.xlsx'
+        
+        self.path_cos_similarity  = './{}/cos_similarity'.format(batch_label)
+        if not os.path.exists(self.path_cos_similarity):
+            os.makedirs(self.path_cos_similarity)
+        self.path_cos_similarity = self.path_cos_similarity + '/{}_vs_{}.xlsx'
+        
         self.adata.obs['qc_cell_idx'] = list(range(len(self.adata.obs)))
         
-        path = './{}/batch_effect_feature'.format(batch_label)
-        if not os.path.exists(path):
-            os.makedirs(path)
+        
             
-        batch = np.unique(self.adata.obs[batch_label])
-        pairs = [[p[0], p[1]] for p in combinations(batch, 2)]
-
-        pool = Pool(num_cores)                        
-        pool.map(self.get_batch_expression_descriptors, batch) 
-        pool.close()
-        pool.join()
+        batches = sorted(list(self.adata.obs[batch_label].value_counts()[self.adata.obs[batch_label].value_counts() > 0].index))
+        pairs = list(combinations(batches, 2))
         
-        
-        path_  = './{}/cos_similarity'.format(batch)
-        if not os.path.exists(path_):
-            os.makedirs(path_)
-        
-        pool = Pool(num_cores)                      
-        pool.map(self._get_batch_cos_sim, pairs) 
-        pool.close()
-        pool.join()
+        if 'qc_cell_idx' not in self.adata.obs.columns:
+            raise ValueError('cell index is not define!')
+        else:
+            print('compute batch-specific feature vector')
+            with Pool(processes = num_cores) as pool:
+                o = list(tqdm(pool.imap_unordered(self._get_batch_feature_vectors, batches), total = len(batches))) 
+            
+            print('compute cosine similarity')
+            with Pool(processes = num_cores) as pool:
+                o = list(tqdm(pool.imap_unordered(self._get_batch_cosine_similarity, pairs), total = len(pairs))) 
 
         cs_dflist = []
-        corr_dflist = []
-        for i, (sample_a, sample_b) in enumerate(pair):
-            filename = sample_a + '_vs_' + sample_b + '.xlsx'
-            df_ = pd.read_excel(os.path.join(path_,filename))
-            cs_dflist.append(df_.iloc[:,1])
-            corr_dflist.append(df_.iloc[:,2])
+        for i, (sample_a, sample_b) in enumerate(pairs):
+            cs_dflist.append(pd.read_excel(self.path_cos_similarity.format(sample_a, sample_b)))
+        
         df_cs = pd.concat(cs_dflist,axis=1)
-        df_cs.index = self.adata[:,self.aprg_mask].var.gene_ids.values
+        df_cs.index = self.adata[:,self.aprg_mask].var[self.gene_id_col_label].values
         df_rbg = df_cs.loc[df_cs.index.str.startswith('RPS') | df_cs.index.str.startswith('RPL')]        
         relative_cs = (df_cs  - df_rbg.mean()).fillna(0).abs()
         
-        variability = pd.DataFrame(relative_cs.sum(axis=1))
-        return variability
+        self.biological_heterogeneity = pd.DataFrame(relative_cs.sum(axis=1), columns = ['var'])
+        self.biological_heterogeneity['qc_gene_idx'] = self.adata[:,self.aprg_mask].var.qc_gene_idx.values
         
-    def select_hvg(self, variability: pd.DataFrame, hvg_cutoff: float = 2.2):
-                
-        hvg_df['qc_gene_idx'] = self.adata[:,self.aprg_mask].var.qc_gene_idx.values
-        adata_hvg = self.adata_apr[:,hvg_df[hvg_df.bio_intensity > 2.2].qc_gene_idx]
+    def select_hvg(self, hvg_min: float = 2.2):
+        adata_hvg = self.adata_apr[:, self.biological_heterogeneity[self.biological_heterogeneity['var'] > hvg_min].qc_gene_idx]
         return adata_hvg
         
-    def dimension_reduction(self, adata_hvg):
+    def dimension_reduction(self, adata_hvg, device:str = 'cpu'):
         adata_hvg = sc.pp.scale(adata_hvg, zero_center=True, max_value=3,copy=True)
-        adata_latent = utils.dim_reduction_ae(adata_hvg)
-        adata.obs['ae'] = adata_latent.obs.autoencoder.values
+        adata_latent = utils.dim_reduction_ae(adata_hvg, device)
+        self.adata.obs['ae'] = adata_latent.obs.autoencoder.values
         
         return adata_latent
